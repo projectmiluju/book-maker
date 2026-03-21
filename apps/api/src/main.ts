914e0426ecc04f535918aadbe6daf510fb1470ea
projectmiluju <dnjsdyd0712@naver.com>
2026-03-21 11:07:41 +0900

    chore: 코드 품질 기준과 스크립트 정리

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,4 +10,4 @@ async function bootstrap() {
 
   await app.listen(port);
 }
-bootstrap();
+void bootstrap();
